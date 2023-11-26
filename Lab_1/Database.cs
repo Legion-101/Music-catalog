ca1e0c8cca2adc0965a9bda563ead0261f67f28e
Legion-101 <kildyaev.ivan@gmail.com>
2023-11-26 23:56:55 +0300

    Rework to from json to database

---
+++ Lab_1/Database.cs
@@ -0,0 +1,71 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Windows.Forms;
+
+namespace Lab_1
+{
+    internal class Database
+    {
+        private NpgsqlConnection connection;
+        private const string CONNECTION_STRING = "Host=localhost:5432;" + "Username=postgres;" + "Password=password;" + "Database=\"music catalog\"";
+
+
+        internal Database()
+        {
+            connection = new NpgsqlConnection(CONNECTION_STRING);
+            connection.Open();
+            
+        }
+
+        ~Database()
+        {
+            connection.Close();
+        }
+
+        internal List<Artist> GetArtists()
+        {
+            List<Artist> list = new List<Artist>();
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT \"Name Artist\", \"Genre\" FROM \"Artists\"", connection);
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Artist artist = new Artist();
+                artist.nameArtist = reader.GetString(0);
+                artist.genre = reader.GetString(1);
+
+                list.Add(artist);
+            }
+            reader.Close();
+            if (list.Count != 0 ) 
+                return list;
+            return null;
+        }
+
+        internal List<Album> GetAlbumArtist(string artist)
+        {
+            List<Album> list = new List<Album>();
+            NpgsqlCommand cmd = new NpgsqlCommand($"SELECT \"Name Artist\", \"Genre\", \"Name album\" FROM \"Albums\" JOIN \"Artists\" ON \"Albums\".\"Id artist\"=\"Artists\".\"Id artist\" WHERE \"Name Artist\"=\'{artist}\'", connection);
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Album album = new Album();
+                album.nameArtist = reader.GetString(0);
+                album.genre = reader.GetString(1);
+                album.nameAlbum = reader.GetString(2);
+                list.Add(album);
+            }
+            if (list.Count != 0)
+                return list;
+            return null;
+        }
+    }
+}
--- Lab_1/MusicCatalog.cs
+++ Lab_1/MusicCatalog.cs
@@ -1,3 +1,4 @@
+using System.DirectoryServices;
 using System.Text.Json;
 using Newtonsoft.Json;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
@@ -7,47 +8,61 @@ namespace Lab_1
     public partial class MusicCatalog : Form
     {
         DataMusicCatalog items = new DataMusicCatalog();
+
+        
         public MusicCatalog()
         {
-            using (StreamReader r = new StreamReader("../../../data.json"))
-            {
-                string json = r.ReadToEnd();
-                items = JsonConvert.DeserializeObject<DataMusicCatalog>(json);
-            }
+            Database database = new Database();
+            List<Artist> artist = database.GetArtists();
+            //List<Album> album = database.GetAlbumArtist(artist[0].nameArtist);
+           
             InitializeComponent();
-            // Print data about Artist
-            for (int i = 0; i < items.Artists.Count; i++)
+            foreach(Artist artistItem in artist)
             {
-                treeView1.Nodes.Add(items.Artists[i].nameArtist);
-                for (int j = 0; j < items.Albums.Count; j++)
+                treeView1.Nodes.Add(artistItem.nameArtist);
+                List<Album> album = database.GetAlbumArtist(artistItem.nameArtist);
+                foreach(Album albumItem in album)
                 {
-                    if (treeView1.Nodes[i].Text == items.Albums[j].nameArtist)
-                    {
-                        treeView1.Nodes[i].Nodes.Add(items.Albums[j].nameAlbum);
-                    }
-                    else continue;
+                    treeView1.Nodes[artistItem.nameArtist].Nodes.Add(albumItem.nameAlbum);
                 }
-                for (int k = 0; k < treeView1.Nodes[i].Nodes.Count;)
-                {
-                    foreach (Track track in items.Albums[k].tracks)
-                    {
-                        if (treeView1.Nodes[i].Nodes[k].Text == items.Albums[k].nameAlbum)
-                            treeView1.Nodes[i].Nodes[k].Nodes.Add(track.nameTrack);
-                        else continue;
-                    }
-                }
+            }
+            //using (StreamReader r = new StreamReader("../../../data.json"))
+            //{
+            //    string json = r.ReadToEnd();
+            //    items = JsonConvert.DeserializeObject<DataMusicCatalog>(json);
+            //}
+            //// Print data about Artist
+            //for (int i = 0; i < items.Artists.Count; i++)
+            //{
+            //    for (int j = 0; j < items.Albums.Count; j++)
+            //    {
+            //        if (treeView1.Nodes[i].Text == items.Albums[j].nameArtist)
+            //        {
+            //            treeView1.Nodes[i].Nodes.Add(items.Albums[j].nameAlbum);
+            //        }
+            //        else continue;
+            //    }
+            //    for (int k = 0; k < treeView1.Nodes[i].Nodes.Count;)
+            //    {
+            //        foreach (Track track in items.Albums[k].tracks)
+            //        {
+            //            if (treeView1.Nodes[i].Nodes[k].Text == items.Albums[k].nameAlbum)
+            //                treeView1.Nodes[i].Nodes[k].Nodes.Add(track.nameTrack);
+            //            else continue;
+            //        }
+            //    }
 
-            }
-            // Print data about collections
-            for (int i = 0; i < items.Collections.Count; i++)
-            {
-                treeViewCollections.Nodes.Add(items.Collections[i].nameCollection);
-                //treeViewCollections.Nodes[i].Nodes.Add(items.Collections[i].tracks[i].genre);
-                for (int j = 0; j < items.Collections[i].tracks.Count; j++)
-                {
-                    treeViewCollections.Nodes[i].Nodes.Add(items.Collections[i].tracks[j].nameTrack);
-                }
-            }
+            //}
+            //// Print data about collections
+            //for (int i = 0; i < items.Collections.Count; i++)
+            //{
+            //    treeViewCollections.Nodes.Add(items.Collections[i].nameCollection);
+            //    //treeViewCollections.Nodes[i].Nodes.Add(items.Collections[i].tracks[i].genre);
+            //    for (int j = 0; j < items.Collections[i].tracks.Count; j++)
+            //    {
+            //        treeViewCollections.Nodes[i].Nodes.Add(items.Collections[i].tracks[j].nameTrack);
+            //    }
+            //}
 
         }
         private void ButtonSearch_Click(object sender, EventArgs e)
